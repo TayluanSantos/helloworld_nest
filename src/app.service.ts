2a3380c3d028e2502198dc41dd5302b7f287835f
TayluanSantos <tayluanjesus@gmail.com>
2024-07-19 11:15:47 -0300

    Aula 01 - Introdução ao NestJS - Hello World

---
+++ src/app.service.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getTurma() : string {
+    return '<i>Turma JavaScript 05</i>';
+  }
+}
